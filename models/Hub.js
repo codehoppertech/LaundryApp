9b7379151e262575e08a8506805101e0b6e7ebb5
Kulraj Chouhan <kulraj89@gmail.com>
2025-02-18 12:21:56 +0000

    18feb

--- models/Hub.js
+++ models/Hub.js
@@ -11,35 +11,35 @@ const hubSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId, 
     ref: "Location", 
     required: true 
-  }, // Reference to Location
+  }, 
   business: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: "Business", 
     required: true 
-  }, // Reference to Business
+  },
   hub_name: {
     type: String,
-    unique: true,
   },
   mac_address: {
     type: String,
-    required: true,
-    unique: true,
+    required: false, // Make it optional
     match: /^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$/, // Validate MAC address format
   },
   serial_number: { 
     type: String, 
-    required: true, 
-    unique: true 
-  }, // Serial number must be unique
+  },
   ports: [
     {
-      position: { type: Number }, // Port position (1, 2, etc.)
-      status: { type: String, enum: ["connected", "not_connected"]}, // Ensures only valid statuses
-      timestamp: { type: Number }// Unix timestamp
-
+      position: { type: Number },
+      status: { type: String, enum: ["connected", "not_connected"]}, 
+      timestamp: { type: Number }
     },
   ],
 });
+
+// Create partial indexes to enforce uniqueness only when the value is not null
+hubSchema.index({ hub_name: 1 }, { unique: true, partialFilterExpression: { hub_name: { $ne: null } } });
+hubSchema.index({ mac_address: 1 }, { unique: true, partialFilterExpression: { mac_address: { $ne: null } } });
+hubSchema.index({ serial_number: 1 }, { unique: true, partialFilterExpression: { serial_number: { $ne: null } } });
 
 module.exports = mongoose.model("Hub", hubSchema);
